f168195436d82e7455546b5a8b987de1dba0bace
Gemellix2322 <guilhermegemelli232@gmail.com>
2024-12-20 11:49:37 -0300

    tentando arrumar css

--- src/Livro.js
+++ src/Livro.js
@@ -1,18 +1,29 @@
-import React from "react";
-import "./Menu.css"
+import React, { useState } from "react";
+import "./Menu.css";
 
-const Livro = ({livro}) => {
-    return (
-        <div className="Livro">
-            <div
-                className="livro-icon"
-                style={{backgroundImage: `url(${livro.image})`}}
-            ></div>
-            <div className="livro-details">
-                <p>{livro.name}</p>
-            </div>
+const Livro = ({ livro }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div className="Livro">
+      <div
+        className="livro-icon"
+        style={{
+          backgroundImage: `url(${livro.image})`,
+          transform: isHovered ? "scale(1.2)" : "scale(1)",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      ></div>
+      {isHovered && (
+        <div className="livro-details">
+          <p>{livro.name}</p>
+          <p>{livro.description}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Livro;
+
