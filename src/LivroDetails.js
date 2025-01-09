9ec37c4e004f0cd685777a8032582dc9d5611883
Gemellix2322 <guilhermegemelli232@gmail.com>
2025-01-09 11:58:23 -0300

    agr sim ta funcionando

--- src/LivroDetails.js
+++ src/LivroDetails.js
@@ -1,12 +1,19 @@
 import { useParams } from 'react-router-dom';
 
-const LivroDetails = ({ users, livros }) => {
+function LivroDetails({ users, livros }) {
+  const { name } = useParams();
+  
+  const livro = livros.find(l => l.name === name);
+
+  if (!livro) {
+    return <div>Livro não encontrado</div>;
+  }
 
   return (
     <div>
-      <h1>FUNCIONA</h1>
+      <h1>{livro.name}</h1>
     </div>
   );
 }
 
-export default LivroDetails;
+export default LivroDetails
