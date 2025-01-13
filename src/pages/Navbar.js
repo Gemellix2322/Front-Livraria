b82a0e5572396f32139ca385961cb539110a7d00
Gemellix2322 <guilhermegemelli232@gmail.com>
2025-01-13 14:52:43 -0300

    change svg color

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,11 +1,23 @@
 import { FiX, FiUser, FiSettings, FiLogOut } from 'react-icons/fi';
 import '../css/Navbar.css'
-import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import insta from "../img/instagram-logo.svg";
 
 const Navbar = ({ user, isNavbarOpen, setIsNavbarOpen }) => {
     const handleClose = () => {
         setIsNavbarOpen(false);
     };
+
+    const currentUser = user[0];
+        
+        const [formData, setFormData] = useState({
+            name: currentUser.username,
+            user: currentUser.name,
+            password: currentUser.password,
+            profile_picture: currentUser.profile_picture,
+        });
+    
 
     return (
         <div className="navbar">
@@ -21,17 +33,34 @@ const Navbar = ({ user, isNavbarOpen, setIsNavbarOpen }) => {
 
                 <div className="sidebar-header">
                     <div className="user-info">
-                        <FiUser size={50} />
-                        <h3>{user?.username || 'Usuário'}</h3>
+                        <Link to='/profile'>
+                            <img src={formData.profile_picture} className='navbar-profile'/>
+                        </Link>
+                        <h3>{formData?.username || 'Usuário'}</h3>
                     </div>
                 </div>
 
                 <div className="sidebar-content">
                     <ul>
-                        <li><FiUser /> Perfil</li>
-                        <li><FiSettings /> Configurações</li>
-                        <li><FiLogOut /> Sair</li>
+                        <Link to='/profile' className='link-icons'>
+                            <li className='sidebar-icons'><FiUser /> Perfil</li>
+                        </Link>
+                        <Link to='/settings' className='link-icons'>
+                            <li className='sidebar-icons'><FiSettings /> Configurações</li>
+                        </Link>
+                        <Link to='/' className='link-icons'>
+                            <li className='sidebar-icons'><FiLogOut /> Sair</li>
+                        </Link>
                     </ul>
+
+                    <Link to='https://www.instagram.com/gemellicafes/' className='link-insta'>
+                        <div className='div-insta'>
+                            <img src={insta} className='insta-logo'/>
+                            <p>
+                                Instagram
+                            </p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
